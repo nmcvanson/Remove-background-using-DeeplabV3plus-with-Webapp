b96779befaea71ddbff185f1656ebfe44476a343
nmcvanson <nmc.vanson.pt@gmail.com>
2022-05-06 17:36:05 +0300

    Add files

---
+++ Build_model/util.py
@@ -0,0 +1,52 @@
+import os
+import numpy as np
+import cv2
+from glob import glob
+from sklearn.utils import shuffle
+import tensorflow as tf
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def shuffling(x, y):
+    x, y = shuffle(x, y, random_state = 11)
+    return x, y
+
+def load_data(path):
+    x = sorted(glob(os.path.join(path, "image", "*png")))
+    y = sorted(glob(os.path.join(path, "mask", "*png")))
+    return x, y
+
+def read_image(path):
+    path = path.decode()
+    x = cv2.imread(path, cv2.IMREAD_COLOR)
+    x = x/255.0
+    x = x.astype(np.float32)
+    return x
+
+def read_mask(path):
+    path = path.decode()
+    x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    x = x.astype(np.float32)
+    x = np.expand_dims(x, axis=-1)
+    return x
+
+def tf_parse(x, y):
+    def _parse(x, y):
+        x = read_image(x)
+        y = read_mask(y)
+        return x, y
+
+    x, y = tf.numpy_function(_parse, [x, y], [tf.float32, tf.float32])
+    x.set_shape([H, W, 3])
+    y.set_shape([H, W, 1])
+    return x, y
+
+def tf_dataset(X, Y, batch=2):
+    dataset = tf.data.Dataset.from_tensor_slices((X, Y))
+    dataset = dataset.map(tf_parse)
+    dataset = dataset.batch(batch)
+    dataset = dataset.prefetch(10)
+    return dataset
